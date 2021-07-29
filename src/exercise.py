a78fb2d6453b23627d04e6284249c6299381401a
Justin Turner <jmturner638@gmail.com>
2021-07-28 20:08:09 -0500

    Second Try

--- src/exercise.py
+++ src/exercise.py
@@ -4,11 +4,11 @@ def main():
     num2 = int(input())
 
     if num1 > num2:
-        print("{} is greater than {}".format(num1, num2))
+        print("{} is greater than {}.".format(num1, num2))
     elif num2 > num1:
-        print("{} is smaller than {}".format(num1, num2))
+        print("{} is smaller than {}.".format(num1, num2))
     else:
-        print("{} is equal to {}".format(num1, num2))
+        print("{} is equal to {}.".format(num1, num2))
 
 if __name__ == '__main__':
     main()
